6cc2d2228b0a46c3e1d8932f388c98e13847f709
Ryou <youdonghoho@hotmail.com>
2020-06-27 11:10:56 -0400

    Update basic.go

--- go/mutex/basic.go
+++ go/mutex/basic.go
@@ -3,6 +3,11 @@ package main
 import "sync"
 import "time"
 
+//NOTE:
+call RPC with mutation on shared state variable is same situation as follow example
+conclusion:
+whenever we a RPC call to mutate a share state, we need a lock
+
 func main() {
 	counter := 0
 	var mu sync.Mutex
