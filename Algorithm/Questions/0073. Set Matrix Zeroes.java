e9fabbee1d4d96b1b79aefe72a5e5558365d5dea
Ryou <youdonghoho@hotmail.com>
2021-09-11 12:01:27 -0400

    Update 0073. Set Matrix Zeroes.java

--- Algorithm/Questions/0073. Set Matrix Zeroes.java
+++ Algorithm/Questions/0073. Set Matrix Zeroes.java
@@ -42,10 +42,25 @@ class Solution {
         int m = matrix.length;
         int n = matrix[0].length;
         
+        // determine whether row and col need to be zero
+        boolean hasFirstColumnZero = false;
+        boolean hasFirstRowZero = false;
         for (int i = 0; i < m; i++) {
             if (matrix[i][0] == 0) {
-                isCol = true;
+                hasFirstColumnZero = true;
             }
+        }
+        
+     
+        
+        for (int j = 0; j < n; j++) {
+            if (matrix[0][j] == 0) {
+                hasFirstRowZero = true;
+            }
+        }
+
+        
+        for (int i = 1; i < m; i++) {
             for (int j = 1; j < n; j++) {
                 if (matrix[i][j] == 0) {
                     matrix[i][0] = 0;
@@ -53,7 +68,6 @@ class Solution {
                 }
             }
         }
-        
         for (int i = 1; i < m; i++) {
             for (int j = 1; j < n; j++) {
                 if (matrix[i][0] == 0 || matrix[0][j] == 0) {
@@ -61,17 +75,18 @@ class Solution {
                 }
             }
         }
-        // See if the first row needs to be set to zero as well
-        if (matrix[0][0] == 0) {
-          for (int j = 0; j < n; j++) {
-            matrix[0][j] = 0;
+
+        // See if the first column needs to be set to zero as well
+        if (hasFirstColumnZero) {
+          for (int i = 0; i < m; i++) {
+            matrix[i][0] = 0;
           }
         }
 
-        // See if the first column needs to be set to zero as well
-        if (isCol) {
-          for (int i = 0; i < m; i++) {
-            matrix[i][0] = 0;
+        // See if the first row needs to be set to zero as well
+        if (hasFirstRowZero) {
+          for (int j = 0; j < n; j++) {
+            matrix[0][j] = 0;
           }
         }
         
