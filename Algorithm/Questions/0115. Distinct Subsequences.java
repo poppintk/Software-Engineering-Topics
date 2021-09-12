4a013219a7cc923a515fd6af4d96f5d5c8fe3978
Ryou <youdonghoho@hotmail.com>
2021-09-12 11:35:25 -0400

    Update 0115. Distinct Subsequences.java

--- Algorithm/Questions/0115. Distinct Subsequences.java
+++ Algorithm/Questions/0115. Distinct Subsequences.java
@@ -1,4 +1,17 @@
 class Solution {
+    /*
+    *
+        For example:
+    t : ab--> ab --> ab --> ab
+    s: a --> ac --> acb --> acbb
+
+    The first case is easy to catch. When the new character in s, s[i-1], is not equal with the head char in t, t[j-1], we can no longer increment the number of distinct subsequences, it is the same as the situation before incrementing the s, so dp[i][j] = dp[i-1][j].
+
+    However, when the new incrementing character in s, s[i-1] is equal with t[j-1], which contains two case:
+
+    We don't match those two characters, which means that it still has original number of distinct subsequences, so dp[i][j] = dp[i-1][j].
+    We match those two characters, in this way. dp[i][j] = dp[i-1][j-1];
+    */
     public int numDistinct(String s, String t) {
         // let dp[i][j] be the number of ways that distinct subsequence of  s[0...i - 1]  and t[0...j - 1]
         // dp[i][j] = dp[i - 1][j - 1] if S[i - 1] == T[j - 1] (match one) 
