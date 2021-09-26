7b9e0ccba0ce4a979f02e1d70f87822eee738c0a
poppintk <youdonghoho@hotmail.com>
2021-09-25 23:39:26 -0400

    Search a 2D matrix II

---
+++ Algorithm/Questions/0240. Search a 2D Matrix II.java
@@ -0,0 +1,20 @@
+class Solution {
+    // O(n)
+    public boolean searchMatrix(int[][] matrix, int target) {
+        
+        int i = matrix.length - 1;
+        int j = 0;
+        
+        while (i >= 0 && j < matrix[0].length) {
+            if (matrix[i][j] < target) {
+                j++;
+            } else if (matrix[i][j] > target) {
+                i--;
+            } else {
+                return true;
+            }
+        }
+        
+        return false;
+    }
+}
