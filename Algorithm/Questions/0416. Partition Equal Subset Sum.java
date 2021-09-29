d662cb905b77951da621665d53a96de23055fc58
poppintk <youdonghoho@hotmail.com>
2021-09-28 20:55:48 -0400

    Partition Equal Subset Sum

---
+++ Algorithm/Questions/0416. Partition Equal Subset Sum.java
@@ -0,0 +1,42 @@
+class Solution {
+    public boolean canPartition(int[] nums) {
+        // similar to 0, 1 backpack
+        int sum = 0;
+        for (int n : nums) {
+            sum += n;
+        }
+        // odd number can not divide into two equal subset
+        if (sum % 2 != 0) return false;
+        
+        // convert into 0/1 backpack problem the weight is sum / 2;
+        // let dp[i][j] be the first item from nums[0...i - 1] that can form a number j 
+        // dp[i][j] = dp[i - 1][j] || dp[i - 1][j - nums[i - 1]]
+        int n = nums.length;
+        int target = sum / 2;
+        boolean[][] dp = new boolean[n + 1][target + 1];
+        
+        for (int i = 0; i < n + 1; i++) {
+            for (int j = 0; j < target + 1; j++) {
+                if (i == 0 && j == 0) {
+                    dp[0][0] = true;
+                    continue;
+                }
+                if (i == 0) {
+                    dp[i][j] = false;
+                    continue;
+                }
+                if (j == 0) {
+                    dp[i][j] = false;// NOTE: many item cannot form a 0 weight
+                    continue;
+                }
+                
+                dp[i][j] = dp[i - 1][j];
+                if (j - nums[i - 1] >= 0) {
+                    dp[i][j] = dp[i][j] || dp[i - 1][j - nums[i - 1]];
+                }
+            }
+        }
+        
+        return dp[n][target];
+    }
+}
