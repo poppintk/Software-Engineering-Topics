e18547c810c29648a6bbe961b67a2fd862eef081
poppintk <youdonghoho@hotmail.com>
2021-10-21 23:03:40 -0400

    Serialize and Deserialize Binary Tree

---
+++ Algorithm/Questions/0297. Serialize and Deserialize Binary Tree.java
@@ -0,0 +1,57 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Codec {
+
+    // Encodes a tree to a single string.
+    public String serialize(TreeNode root) {
+        StringBuilder sb = new StringBuilder();
+        serializeHelper(root, sb);
+        return sb.toString();
+    }
+    
+    private void serializeHelper(TreeNode root, StringBuilder sb){
+        if(root==null){
+            sb.append("null").append(",");
+            return;
+        }
+        
+        sb.append(root.val).append(",");
+        serializeHelper(root.left, sb);
+        serializeHelper(root.right, sb);
+    }
+
+    // Decodes your encoded data to tree.
+    public TreeNode deserialize(String data) {
+       // System.out.println(data);
+        Queue<String> queue = new LinkedList<>(Arrays.asList(data.split(",")));
+        return deserializeHelper(queue);
+    }
+    
+    private TreeNode deserializeHelper(Queue<String> queue){
+        if(queue.isEmpty()){
+            return null;
+        }
+        
+        String current = queue.poll();
+        if(current.equals("null"))
+            return null;
+        
+        
+        TreeNode root = new TreeNode(Integer.parseInt(current));
+        root.left = deserializeHelper(queue);
+        root.right = deserializeHelper(queue);
+        
+        return root;
+    }
+}
+
+// Your Codec object will be instantiated and called as such:
+// Codec codec = new Codec();
+// codec.deserialize(codec.serialize(root));
