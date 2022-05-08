1fce994be221b06048b8889eb80c8201f3e443db
Ryou <youdonghoho@hotmail.com>
2022-05-07 20:03:22 -0400

    Update Singleton4.java

--- Design pattern/singleton/Singleton4.java
+++ Design pattern/singleton/Singleton4.java
@@ -1,11 +1,11 @@
 package singleton;
 
 public class Singleton3 {
-    private static Singleton3 uniqueInstance = new Singleton3(); // create instance at runtime
+    private static Singleton3 uniqueInstance = new Singleton3(); // create instance at initial runtime
 
     private Singleton3(){}
     
-    public static synchronized Singleton3 getInstance(){
+    public static Singleton3 getInstance(){
         return uniqueInstance;
     }
 
