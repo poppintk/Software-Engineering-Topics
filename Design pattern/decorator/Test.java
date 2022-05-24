4e70ed5e2603bc641cf1669d5ce6b44920a69479
Ryou <youdonghoho@hotmail.com>
2022-05-24 11:39:28 -0400

    Update Test.java

--- Design pattern/decorator/Test.java
+++ Design pattern/decorator/Test.java
@@ -5,7 +5,7 @@ public class Test {
     public static void main(String[] args){
         Beverage beverage = new Espresso();
 
-        beverage = new Mocha(beverage); // Note: Mocha and Soy are concrete decorator type, 
+        beverage = new Mocha(beverage); // Note: Mocha and Soy are concrete class of Decorator abstracted class 
         beverage = new Mocha(beverage); // Decorator is abstracted class has internal member variable of Beverage(target to be decorated) which also inherited Beverage type 
         beverage = new Soy(beverage);
 
